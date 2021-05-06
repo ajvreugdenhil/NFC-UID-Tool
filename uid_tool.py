c2013829bac072e65fbcebb6eddec957e811c673
Arjan <a.j.vreugdenhil2000@gmail.com>
2021-05-06 15:05:38 +0200

    some cleanup

--- uid_tool.py
+++ uid_tool.py
@@ -84,87 +84,14 @@ def write_uid_desfire(newuid: str):
     _write(desfire_backdoor_command_two)
     _write(write_command)
 
-def write_uid_mfclassic(uid):
-    print("We recommend you use nfc-mfsetuid for now")
-    raise NotImplemented
-    uid = sys.argv[2]
-    uid_values = bytes.fromhex(uid)
-    assert len(uid_values) == 4
-    
-    original_blockzero = [0xCA, 0xFE, 0xBA, 0xBE, 0x30, 0x08, 0x04, 0x00, 0x46, 0x59, 0x25, 0x58, 0x49, 0x10, 0x23, 0x02]
-    example_write_to_zero = [0x01,  0xff,  0xff,  0xff,  0xfe,  0x08, 0x04,  0x00,  0x46,  0x59,  0x25,  0x58,  0x49,  0x10,  0x23,  0x02,  0x39,  0x08]
-
-    blocknr = 0x0
-    write_command = [0xFF, 0xD6, 0x00, blocknr, 0x7]
-    for b in original_blockzero:
-        write_command.append(b)
-    
-    #write_command = [0xff, 0x00, 0x00, 0x00, 0x14, 0xd4, 0x42, 0x41, 0x41, 0x41, 0x41, 0x00, 0x08, 0x04, 0x00, 0x46, 0x59, 0x25, 0x58, 0x49, 0x10, 0x23, 0x02, 0xe9, 0x60]
-    backdoor_command_zero = [0xff, 0x00, 0x00, 0x00, 0x06, 0xd4, 0x42, 0x50, 0x00, 0x57, 0x00]
-    backdoor_command_one = [0xff, 0x00, 0x00, 0x00, 0x05, 0xd4, 0x08, 0x63, 0x3d, 0x00]
-    #backdoor_command_one = [0xff, 0x00, 0x00, 0x00, 0x03, 0xd4, 0x42, 0x40]
-    backdoor_command_two = [0xff, 0x00, 0x00, 0x00, 0x03, 0xd4, 0x42, 0x43]
-    backdoor_command_three = [0xff, 0x00, 0x00, 0x00, 0x06, 0xd4, 0x42, 0xa0, 0x00, 0x5f, 0xb1]
-    _write(backdoor_command_one)
-    _write(backdoor_command_two)
-    _write(backdoor_command_three)
-    _write([0xff, 0x00, 0x00, 0x00, 0x14, 0xd4, 0x42] + example_write_to_zero)
-    '''
-    behavior of nfc-mfsetuid:
-                d5 43 0041414141009000 # answer, this is my uid
-
-    ff000000 0b  d4 42 93704141414100e21a # select?
-    ff000000 06  d4 42 500057cd
-    ff000000 04  d4 06 633d
-    ff000000 05  d4 08 633d07
-    ff000000 03  d4 42 40
-    ff000000 04  d4 06 633c
-    ff000000 04  d4 06 633d
-    ff000000 05  d4 08 633d00
-    ff000000 03  d4 42 43
-    ff000000 06  d4 42 a0005fb1
-    ff000000 14  d4 42 cafebabe300804004659255849102302a064
-    ff000000 02  d4 02
-    ff000000 03  d4 52 00
-    ff000000 04  d4 32 0100
-    '''
-    '''
-    00  00  00  00  00  08  04  00  46  59  25  58  49  10  23  02  1a  f0
-    00  00  00  01  01  08  04  00  46  59  25  58  49  10  23  02  e2  70
-    00  00  00  02  02  08  04  00  46  59  25  58  49  10  23  02  fb  f9
-    00  00  00  03  03  08  04  00  46  59  25  58  49  10  23  02  03  79
-    00  00  00  04  04  08  04  00  46  59  25  58  49  10  23  02  d8  e3
-    00  00  00  ff  ff  08  04  00  46  59  25  58  49  10  23  02  0e  c0
-    00  00  01  00  01  08  04  00  46  59  25  58  49  10  23  02  a5  0b
-    00  00  01  ff  fe  08  04  00  46  59  25  58  49  10  23  02  b1  3b
-    00  00  ff  ff  00  08  04  00  46  59  25  58  49  10  23  02  8f  a6 
-    00  ff  ff  ff  ff  08  04  00  46  59  25  58  49  10  23  02  7c  03
-    01  ff  ff  ff  fe  08  04  00  46  59  25  58  49  10  23  02  39  08
-    |------------|
-    uid
-                    |
-                    xor uid vals
-    '''
-
-
 
 def main():
     logger.error("Be careful! Somewhere along the line I borked my only 7B card. This current code is not known-good")
-    
     '''
     write_uid_desfire("aabbccddeeff11")
     uid = get_uid()
     print("UID: " + ' '.join('{:02x}'.format(x) for x in uid))
     '''
-
-    '''
-    uid[0] += 1
-    write_uid_desfire(''.join('{:02x}'.format(x) for x in uid))
-
-    uid = get_uid()
-    logger.info("UID: " + ' '.join('{:02x}'.format(x) for x in uid))
-    '''
-
 
 
 if __name__ == "__main__":
